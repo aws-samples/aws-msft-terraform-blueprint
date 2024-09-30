f4759264d780ef361cb614e188c159706505b4c4
Mo Mohoboob <momohob@amazon.com>
2024-09-30 12:16:10 +0100

    updated as provider

--- workshops/ENT303/main.tf
+++ workshops/ENT303/main.tf
@@ -4,17 +4,16 @@ terraform {
     aws = {
 
       source  = "hashicorp/aws"
-      version = "~> 3.70"
+      version = "~> 5.0"
 
     }
   }
 
-  required_version = ">= 0.14.9"
+  required_version = ">= 1.0.0"
 }
 
 provider "aws" {
 
-  profile = "default"
   region  = var.region
 
   default_tags {
